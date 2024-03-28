bc8cd2e7380c7f4f777966f8848745786ebbbbf6
Jaromír Smrček <4plague@gmail.com>
2024-03-28 15:34:22 +0100

    Update osc-build for new Dockerfile

--- osc-build.sh
+++ osc-build.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 
-set -x
+set -e
 
-[ -z "$MTR_GITLAB_LOGIN" ] && exit 2
-[ -z "$MTR_GITLAB_HOST" ] && exit 3
-[ -z "$MTR_GITLAB_PASSWORD" ] && exit 4
+function die() {
+	echo "ERROR: $1" 1>&2
+	exit 1
+}
 
-if [ ! -f ./github_token ]; then
-	if [ -z "$GITHUB_TOKEN" ]; then
-		echo "missing mandatory github_token"
-		exit 5
-	else
-		echo $GITHUB_TOKEN > ./github_token
-	fi
-fi
+[ -z "$MTR_GITLAB_LOGIN" ] && die "Unset MTR_GITLAB_LOGIN"
+[ -z "$MTR_GITLAB_HOST" ] && die "Unset MTR_GITLAB_HOST"
+[ -z "$MTR_GITLAB_PASSWORD" ] && die "Unset MTR_GITLAB_PASSWORD"
 
 DOCKER_CRE=podman # or docker
+
 COMMITID=$(git rev-parse --short HEAD)
 #COMMITDATE=$(git show -s --format=%cd --date=format:'%d%m%Y' $COMMITID)
 COMMITDATE=$(date +%d%m%Y)
-MTRPREFIX="osc/onmetal"
-# Component name (for publishing in MTR)
-CNAME='dp-service'
+TAG="osc/onmetal/dp-service:$COMMITDATE-$COMMITID"
 
-#--secret=id=github_token,src=../../github_token
-$DOCKER_CRE build --platform=linux/amd64 --build-arg="DPSERVICE_FEATURES=-Denable_virtual_services=true" --secret=id=github_token,src=./github_token -t $MTRPREFIX/$CNAME:$COMMITDATE-$COMMITID .
+echo "$MTR_GITLAB_PASSWORD" | $DOCKER_CRE login --username "$MTR_GITLAB_LOGIN" --password-stdin "$MTR_GITLAB_HOST"
 
-echo "${MTR_GITLAB_PASSWORD}" | $DOCKER_CRE login --username "${MTR_GITLAB_LOGIN}" --password-stdin "${MTR_GITLAB_HOST}"
+$DOCKER_CRE build --build-arg="DPSERVICE_FEATURES=-Denable_virtual_services=true" -t "$TAG" .
 
-$DOCKER_CRE tag $MTRPREFIX/$CNAME:$COMMITDATE-$COMMITID "$MTR_GITLAB_HOST/$MTRPREFIX/$CNAME:$COMMITDATE-$COMMITID"
-$DOCKER_CRE push -q "$MTR_GITLAB_HOST/$MTRPREFIX/$CNAME:$COMMITDATE-$COMMITID"
+$DOCKER_CRE tag "$TAG" "$MTR_GITLAB_HOST/$TAG"
+$DOCKER_CRE push -q "$MTR_GITLAB_HOST/$TAG"
+$DOCKER_CRE logout "$MTR_GITLAB_HOST"
 
 # uncomment this if you want to reconfigure MTR image as public; then also MTR_GITLAB_TOKEN needs to be defined
 #curl -H "Authorization: Bearer $MTR_GITLAB_TOKEN" -H 'Content-Type: application/json' -XPOST https://${MTR_GITLAB_HOST}/api/v1/repository/${target_basename}/changevisibility -d '{"visibility": "public"}'
-
-
-
