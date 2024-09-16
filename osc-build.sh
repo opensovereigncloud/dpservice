daf70af057bb028593d69f99363fc6411b083284
Florin Peter <public@florin-peter.de>
2024-09-16 06:56:12 +0200

    Streamline build process and clean up Dockerfile
    Disabled unnecessary variable checks and authentication steps in `osc-build.sh` to simplify the script. Modified Dockerfile to remove extended build and test-image steps, focusing only on the production image. These changes aim to optimize the build process and reduce overhead.

--- osc-build.sh
+++ osc-build.sh
@@ -7,23 +7,24 @@ function die() {
 	exit 1
 }
 
-[ -z "$MTR_GITLAB_LOGIN" ] && die "Unset MTR_GITLAB_LOGIN"
+#[ -z "$MTR_GITLAB_LOGIN" ] && die "Unset MTR_GITLAB_LOGIN"
 [ -z "$MTR_GITLAB_HOST" ] && die "Unset MTR_GITLAB_HOST"
-[ -z "$MTR_GITLAB_PASSWORD" ] && die "Unset MTR_GITLAB_PASSWORD"
+#[ -z "$MTR_GITLAB_PASSWORD" ] && die "Unset MTR_GITLAB_PASSWORD"
 
-DOCKER_CRE=podman # or docker
+DOCKER_CRE=docker
 
-COMMITID=$(git rev-parse --short HEAD)
+#COMMITID=$(git rev-parse --short HEAD)
+COMMITID=$(date +%s)
 #COMMITDATE=$(git show -s --format=%cd --date=format:'%d%m%Y' $COMMITID)
 COMMITDATE=$(date +%d%m%Y)
 TAG="$MTR_GITLAB_HOST/osc/onmetal/dp-service:$COMMITDATE-$COMMITID"
 
-echo "$MTR_GITLAB_PASSWORD" | $DOCKER_CRE login --username "$MTR_GITLAB_LOGIN" --password-stdin "$MTR_GITLAB_HOST"
+#echo "$MTR_GITLAB_PASSWORD" | $DOCKER_CRE login --username "$MTR_GITLAB_LOGIN" --password-stdin "$MTR_GITLAB_HOST"
 
-$DOCKER_CRE build --platform=linux/amd64 --build-arg=DPSERVICE_FEATURES="-Denable_virtual_services=true -Denable_pf1_proxy=true" -t "$TAG" .
+$DOCKER_CRE build --platform=linux/amd64 --network=host --build-arg=TARGETARCH=amd64 --build-arg=DPSERVICE_FEATURES="-Denable_virtual_services=true" -t "$TAG" .
 
 $DOCKER_CRE push "$TAG"
-$DOCKER_CRE logout "$MTR_GITLAB_HOST"
+#$DOCKER_CRE logout "$MTR_GITLAB_HOST"
 
 # uncomment this if you want to reconfigure MTR image as public; then also MTR_GITLAB_TOKEN needs to be defined
 #curl -H "Authorization: Bearer $MTR_GITLAB_TOKEN" -H 'Content-Type: application/json' -XPOST https://${MTR_GITLAB_HOST}/api/v1/repository/${target_basename}/changevisibility -d '{"visibility": "public"}'
